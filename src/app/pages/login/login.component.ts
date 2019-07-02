ab82b29034a7704001b8f6493d953a6e8f1d8ad0
Joel Barba <joel.barba.vidal@gmail.com>
2019-07-02 18:38:18 +0100

    First login test

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { auth } from 'firebase/app';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public username: string;
+  public password: string;
+
+  public newMail: string;
+  public newPass: string;
+
+  constructor(
+    public afAuth: AngularFireAuth
+  ) { }
 
   ngOnInit() {
+    console.log('app', this.afAuth.auth.app);
+    console.log('currentUser', this.afAuth.auth.currentUser);
+
+  }
+
+  public createUser = () => {
+    console.log('eee');
+
+
+    this.afAuth.auth.createUserWithEmailAndPassword(this.newMail, this.newPass)
+    .catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode == 'auth/weak-password') {
+    alert('The password is too weak.');
+    } else {
+    alert(errorMessage);
+    }
+    console.log(error);
+    });
   }
 
 }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,11 +1,16 @@
 import { LoginComponent } from './login.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { GlobalsModule } from 'src/app/globals/globals.module';
+
 
 @NgModule({
   declarations: [LoginComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    GlobalsModule,
+    FormsModule,
   ],
   exports: [LoginComponent]
 })
